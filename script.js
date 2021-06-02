c73d8dc76e3967510aae46b4534724d498b77588
Noor Alhomsi <nalhomsi@gmail.com>
2021-06-02 06:54:42 -0400

    draggable

--- script.js
+++ script.js
@@ -103,4 +103,16 @@ $(document).ready(function(){
     initPage()
   }) 
 
+  // select the item element
+const item = document.querySelector('.item');
+
+// attach the dragstart event handler
+item.addEventListener('dragstart', dragStart);
+
+// handle the dragstart
+
+function dragStart(e) {
+   console.log('drag starts...');
+}
+
 });
