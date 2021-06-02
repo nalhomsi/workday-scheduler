bb1956b944d47f5fe24619ef5954e1bc6c8d2d62
Noor Alhomsi <nalhomsi@gmail.com>
2021-06-02 06:37:03 -0400

    hours updated

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 var currentHour = moment().format('h:mm:ss a');
 // Text hour var
-var sixAm = $("#6am");
-var sevenAm = $("#7am");
-var eightAm = $("#8am");
 var nineAm = $("#9am");
 var tenAm = $("#10am");
 var elevenAm = $("#11am");
@@ -13,6 +10,9 @@ var onePm = $("#13pm");
 var twoPm = $("#14pm");
 var threePm = $("#15pm");
 var fourPm = $("#16pm");
+var fivePm = $("#17pm");
+var sixPm = $("#18pm");
+var sevenPm = $("#19pm");
 
 var hour = moment().hours();
 var userInput;
@@ -32,37 +32,37 @@ var interval = setInterval(function() {
 function initPage() {
 
   console.log("Current Hour " + hour);
-  var init9 = JSON.parse(localStorage.getItem("06:00 am"));
+  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
   nineAm.val(init9);
 
-  var init10 = JSON.parse(localStorage.getItem("07:00 am"))
+  var init10 = JSON.parse(localStorage.getItem("10:00 am"))
   tenAm.val(init10);
   
-  var init11 = JSON.parse(localStorage.getItem("08:00 am"))
+  var init11 = JSON.parse(localStorage.getItem("11:00 am"))
   elevenAm.val(init11);
   
-  var init12 = JSON.parse(localStorage.getItem("09:00 am"))
+  var init12 = JSON.parse(localStorage.getItem("12:00 pm"))
   twelvePm.val(init12);
   
-  var init1 = JSON.parse(localStorage.getItem("10:00 am"))
+  var init1 = JSON.parse(localStorage.getItem("01:00 pm"))
   onePm.val(init1);
   
-  var init2 = JSON.parse(localStorage.getItem("11:00 am"))
+  var init2 = JSON.parse(localStorage.getItem("02:00 pm"))
   twoPm.val(init2);
   
-  var init3 = JSON.parse(localStorage.getItem("12:00 pm"))
+  var init3 = JSON.parse(localStorage.getItem("03:00 pm"))
   threePm.val(init3);
  
-  var init4 = JSON.parse(localStorage.getItem("01:00 pm"))
+  var init4 = JSON.parse(localStorage.getItem("04:00 pm"))
   fourPm.val(init4);
   
-  var init5 = JSON.parse(localStorage.getItem("02:00 pm"))
+  var init5 = JSON.parse(localStorage.getItem("05:00 pm"))
   fivePm.val(init5);
   
-  var init6 = JSON.parse(localStorage.getItem("03:00 pm"))
+  var init6 = JSON.parse(localStorage.getItem("06:00 pm"))
   sixPm.val(init6);
   
-  var init7 = JSON.parse(localStorage.getItem("04:00 pm"))
+  var init7 = JSON.parse(localStorage.getItem("07:00 pm"))
   sevenPm.val(init7);
 } 
 
@@ -89,7 +89,6 @@ $(document).ready(function(){
   background()
 
   // Buttons (save to Local Storage)
-
   $(".saveBtn").on("click", function(){
     userInput = $(this).siblings(".form-control").val().trim();
     console.log(userInput);
@@ -99,7 +98,6 @@ $(document).ready(function(){
 
   })
   // Button for clear the day
-  
   $("#clearDay").on("click", function(){
     localStorage.clear();
     initPage()
